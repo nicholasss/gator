c73037d473764d4cdcfc9aa0215d9cb68cb2a4eb
Nicholas <nicholaszingleman@gmail.com>
2025-02-18 12:07:36 -0500

    updated fetch feeds query to return the list instead of just one

--- sql/queries/feeds.sql
+++ sql/queries/feeds.sql
@@ -34,7 +34,6 @@ set last_fetched_at = $2,
 	updated_at = $2
 where id = $1;
 
--- name: GetNextFeedToFetch :one
+-- name: GetNextFeedToFetch :many
 select * from feeds
-	order by last_fetched_at asc nulls first
-	limit 1;
+	order by last_fetched_at asc nulls first;
